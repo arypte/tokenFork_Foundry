ddf349f437e3b6e3623b2bbf4bdfa23063307c7a
arypte <wjs505@gmail.com>
2024-11-10 10:56:16 +0900

    feat: FeeVault init

--- src/FeeVault.sol
+++ src/FeeVault.sol
@@ -1,12 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.11;
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { ERC20Burnable } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract FeeVault is Ownable{
+import { Initializable } from "./abstract/Initializable.sol";
+
+contract FeeVault is Ownable, Initializable{
+
+    function initialize() external initializer {
+        _transferOwnership(_msgSender());
+    }
 
     function makeAndBurnLp() external onlyOwner(){
-
         _sellSFTwithDEX();
     }
 
